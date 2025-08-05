6609a2b35b6d982b3b7288acbdc71dbf34a10a5e
Adrià <128262424+airdac@users.noreply.github.com>
2025-08-05 11:06:24 +0200

    Add painting and resize features

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,48 @@
-const container = document.querySelector(".container");
-const containerWidth = parseFloat(getComputedStyle(container).width);
+const grid = document.querySelector(".grid");
+const gridWidth = parseFloat(getComputedStyle(grid).width);
 
-const squaresPerSide = 4;
+let gridSize = 4;
+const maxGridSize = 40;
 
-for (let i = 0; i < squaresPerSide ** 2; i++) {
-  let square = document.createElement("div");
-  square.classList.add("square");
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize ** 2; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
 
-  let squareSide = Math.floor(containerWidth / squaresPerSide) + 2;
-  square.style.width = `${squareSide}px`;
+    let squareSide = Math.floor(gridWidth / gridSize);
+    square.style.width = `${squareSide}px`;
 
-  container.appendChild(square);
+    // Remove extra borders
+    if (i < gridSize) square.style.borderTop = "0";
+    if (i % gridSize === 0) square.style.borderLeft = "0";
+
+    grid.appendChild(square);
+    }
 }
+
+function empty(grid) {
+    while (grid.lastChild) {
+        grid.lastChild.remove();
+    }
+}
+
+createGrid(gridSize);
+
+// Add hovering effect
+grid.addEventListener("mouseover", (event) => {
+    const target = event.target;
+
+    if (target.classList.contains("square")) {
+        target.style.background = "black";
+    }
+});
+
+const resizeButton = document.querySelector("button");
+
+resizeButton.addEventListener('click', () => {
+    do {gridSize = parseInt(prompt(`Choose the size of the grid's sides (max. ${maxGridSize}).`));
+    } while (isNaN(gridSize) || gridSize > maxGridSize)
+
+    empty(grid);
+    createGrid(gridSize);
+});
